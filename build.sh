ff985c75f79ab5e6847657c9e6d1fac1a79bb813
PRUDHVI0143 <PRUDHVI0143@users.noreply.github.com>
2026-05-05 18:43:59 +0530

    Auto-populate products and guest user on deploy

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ pip install -r requirements.txt
 cd flipko/backend
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Populate database with products and guest user
+python scripts/recover_products.py
+python scripts/create_guest.py
