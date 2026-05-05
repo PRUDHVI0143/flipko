68f74bc6aa3ac7883068aa69ae00c1030617bfd4
PRUDHVI0143 <PRUDHVI0143@users.noreply.github.com>
2026-05-05 18:26:26 +0530

    Allow Render hosts in settings.py

--- flipko/backend/flipko/settings.py
+++ flipko/backend/flipko/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-7-!a5jip$_j(%p-@9g6bm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,.onrender.com').split(',')
 
 
 # Application definition
